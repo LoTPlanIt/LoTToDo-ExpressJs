61d9aa5f134e7911305db1553526fb84dfc4a03b
Thidtanai <thidsloth@gmail.com>
2025-01-20 16:44:58 +0700

    refactor: :building_construction: rename features folder to apis

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const { swaggerUi, swaggerSpec } = require('./configs/swagger.config')
-const apiV1Router = require('./features/v1/apiV1.route')
+const apiV1Router = require('./apis/v1/apiV1.route')
 
 class AppRoutes {
   static routes() {
